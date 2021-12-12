c26b0cc7279ab1444a69b8464c079aee944365df
Anjali Soni <anjalipatadiya@gmail.com>
2021-12-11 18:39:20 -0700

    Add a function that counts characters for tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('textarea').on('input', function() {
+    let lengthOfWords = $(this).val().length;
+    let $counter = $(this).parent('form').find('.counter');
+    updateCountdown($counter, lengthOfWords);
+  });
+});
+function updateCountdown($counter, len) {
+  let charsLeft = 140 - len;
+  $counter.text(charsLeft);
+  $counter.css('color', 'black');
+  if (charsLeft < 0) {
+    $counter.css('color', 'red');
+  }
+}
+
